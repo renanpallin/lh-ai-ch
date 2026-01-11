649cbe7252cdcccf21b6a6653733b43b898b2dd3
Renan Pallin <renanpallin@gmail.com>
2026-01-11 17:29:10 -0300

    Add warning when database not reached

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,9 +2,7 @@ import os
 
 
 class Settings:
-    DATABASE_URL: str = os.getenv(
-        "DATABASE_URL",
-    )
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     UPLOAD_DIR: str = os.getenv("UPLOAD_DIR", "/tmp/docproc_uploads")
 
